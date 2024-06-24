5356e84ff6587b3d32fd400046d8ae91e1678e27
Priyanshi Kaur <167847176+PriyanshiKaurJi@users.noreply.github.com>
2024-06-24 18:18:34 +0530

    Update goiadminv2.js

--- scripts/cmds/goiadminv2.js
+++ scripts/cmds/goiadminv2.js
@@ -12,16 +12,16 @@ module.exports = {
 	onStart: async function () {},
 	onChat: async function ({ event, message, getLang, api }) {
 		const msg = [
-			"Stop mentioning my creator, he's busy 😗",
+			"Stop mentioning my creator, She is busy 😗",
 			"My Creator is currently offline 😢",
 			"𝖠𝗇𝗈𝗍𝗁𝖾𝗋 𝗍𝖺𝗀 𝗂𝗇 𝗆𝗒 𝖺𝖽𝗆𝗂𝗇, 𝗂 𝗐𝗂𝗅𝗅 𝗉𝗎𝗇𝖼𝗁 𝗒𝗈𝗎 🙂",
-			"busy pa ata yun kaya mag-antay ka",
-			"Sorry, naka bebetime pa don't disturb him 🙄",
-			"Do you like my creator thats why your tagging him? Why dont you add him https://www.facebook.com/swordigo.swordslush 😏",
+			"Busy lady she is not here",
+			"Sorry, It's her bedtime don't disturb her 🙄",
+			"Do you like my creator thats why your tagging her? Why dont you add her https://www.facebook.com/PriyanshiKaurJi 😏",
 			"Another tag in my Creator, i will kick your fucking ass"
 		];
 
-		const CliffRegex = /^(@Cliff Vincent V. Torrevillas 󱢏)$/i;
+		const CliffRegex = /^(@Priyanshi Kaur󱢏)$/i;
 		if (event.body && CliffRegex.test(event.body)) {
 			api.setMessageReaction("😍", event.messageID, (err) => {}, true);
 			return api.sendMessage({ body: msg[Math.floor(Math.random() * msg.length)] }, event.threadID, event.messageID);

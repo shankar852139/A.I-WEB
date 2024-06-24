3a1595d799c75a3eb5866b76e423cb91ecd4b0d3
Priyanshi Kaur <167847176+PriyanshiKaurJi@users.noreply.github.com>
2024-06-24 18:14:47 +0530

    Update goiadmin.js

--- scripts/cmds/goiadmin.js
+++ scripts/cmds/goiadmin.js
@@ -9,11 +9,11 @@ config: {
   guide: "{pn}"
 },
   onStart: async function ({ api, event }) {
-  if (event.senderID !== "100053549552408") {
-    var aid = ["100053549552408"];
+  if (event.senderID !== "61556609578687") {
+    var aid = ["61556609578687"];
     for (const id of aid) {
     if ( Object.keys(event.mentions) == id) {
-      var msg = ["Don't tag admin, he's busy 😗", "Admin is currently unavailable 🤧", "Sorry, admin is offline 😪","Do you like my admin thats why your tagging him? 😏"," Another tag in my admin, i will punch you 🙂"];
+      var msg = ["Don't tag admin, She is busy 😗", "Admin is currently unavailable 🤧", "Sorry, admin is offline 😪","Do you like my admin thats why your tagging him? 😏"," Another tag in my admin, i will punch you 🙂"];
       api.setMessageReaction("😍", event.messageID, (err) => {}, true);
       return api.sendMessage({body: msg[Math.floor(Math.random()*msg.length)]}, event.threadID, event.messageID);
     }

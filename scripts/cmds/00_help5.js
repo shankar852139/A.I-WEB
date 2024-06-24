3bf278a352d86a67608db0995b5885dbdc6bc226
Priyanshi Kaur <167847176+PriyanshiKaurJi@users.noreply.github.com>
2024-06-24 17:51:19 +0530

    Update 00_help5.js

--- scripts/cmds/00_help5.js
+++ scripts/cmds/00_help5.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "[ 🐐 | GoatBot V2 ]";
+const doNotDelete = "[ ♕︎ | QueenBot V2 ]";
 
 module.exports = {
 	config: {

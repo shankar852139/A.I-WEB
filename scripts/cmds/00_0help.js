1f5170f900ec251f72b7f2370271253c41200494
Priyanshi Kaur <167847176+PriyanshiKaurJi@users.noreply.github.com>
2024-06-24 17:48:52 +0530

    Update 00_0help.js

--- scripts/cmds/00_0help.js
+++ scripts/cmds/00_0help.js
@@ -4,7 +4,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "⾕ | 𝙔𝘼𝙕𝙆𝙔 𝘼𝙄";
+const doNotDelete = "𝑷𝒓𝒊𝒚𝒂𝒏𝒔𝒉𝒊 𝑲𝒂𝒖𝒓";
 /** 
 * @author NTKhang
 * @author: do not delete it

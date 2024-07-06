2b1f1ad63112f718e7c5e6ea205318db50974d89
Priyanshi Kaur <167847176+PriyanshiKaurJi@users.noreply.github.com>
2024-07-07 00:49:17 +0530

    Update guessnumber.js

--- scripts/cmds/guessnumber.js
+++ scripts/cmds/guessnumber.js
@@ -509,4 +509,147 @@ function guessNumberGame(options) {
           xOutSide,
           yOutSide,
           sizeOfOneSquare,
+          sizeOfOneSquare,
+          radius,
+          lineWidth,
+          '#919191',
+          true
+        );
+
+        drawBorderSquareRadius(
+          ctx,
+          xInSide,
+          yInSide,
+          sizeOfOneSquare - lineWidth * 2,
+          sizeOfOneSquare - lineWidth * 2,
+          radius / 2,
+          lineWidth,
+          backgroundColor,
+          true
+        );
+      }
+    }
+  }
+
+  if (!canvasHightLight) {
+    // if there's no canvasHightLight, then of course ctxHightLight, canvasNumbers and ctxNumbers doesn't either
+    canvasHightLight = createCanvas(canvas.width, canvas.height);
+    ctxHightLight = canvasHightLight.getContext('2d');
+    canvasNumbers = createCanvas(canvas.width, canvas.height);
+    ctxNumbers = canvasNumbers.getContext('2d');
+  }
+
+  // draw numbers
+  let isWin = null;
+  if (numbers.length) {
+    ctxNumbers.font = fontNumbers;
+    ctxNumbers.fillStyle = '#f0f0f0';
+    ctxNumbers.textAlign = 'center';
+    ctxNumbers.textBaseline = 'middle';
+    for (let i = 0; i < col; i++) {
+      const { xOutSide, yOutSide, xInSide, yInSide } = getPositionOfSquare(i, tryNumber, sizeOfOneSquare, distance, marginX, marginY, lineWidth, heightGameName);
+      // draw background of square
+      drawBorderSquareRadius(
+        ctx,
+        xInSide,
+        yInSide,
+        sizeOfOneSquare - lineWidth * 2,
+        sizeOfOneSquare - lineWidth * 2,
+        radius / 2,
+        lineWidth,
+        '#a3a3a3',
+        true
+      );
+      // draw number
+      const x = xOutSide + sizeOfOneSquare / 2;
+      const y = yOutSide + sizeOfOneSquare / 2;
+      ctxNumbers.fillText(numbers[i], x, y);
+
+      // yellow || green 
+      if (
+        answer.includes(numbers[i]) // yellow (correct number)
+        || numbers[i] === answer[i] // green (correct number and position)
+      ) {
+        drawBorderSquareRadius(
+          ctxHightLight,
+          xOutSide,
+          yOutSide,
+          sizeOfOneSquare,
+          sizeOfOneSquare,
+          radius,
+          lineWidth,
+          numbers[i] == answer[i] ? '#417642' : '#A48502',
+          true
+        );
+        drawBorderSquareRadius(
+          ctxHightLight,
+          xInSide,
+          yInSide,
+          sizeOfOneSquare - lineWidth * 2,
+          sizeOfOneSquare - lineWidth * 2,
+          radius / 2,
+          lineWidth,
+          numbers[i] == answer[i] ? '#57AC58' : '#E9BE00',
+          true
+        );
+      }
+    }
+
+    // After each guess, you will get additional hints of the number of correct digits (shown on the left) and the number of correct digits (shown on the right).
+    let numberRight = 0;
+    let numberRightPosition = 0;
+    answer.split('').forEach((item, index) => {
+      if (numbers.includes(item))
+        numberRight++;
+      if (item == numbers[index])
+        numberRightPosition++;
+    });
+
+    ctx.font = fontSuggest;
+    ctx.fillText(numberRight, marginX / 2, marginY + sizeOfOneSquare / 2 + heightGameName + tryNumber * (sizeOfOneSquare + distance));
+    ctx.fillText(numberRightPosition, marginX + col * (sizeOfOneSquare) + distance * (col - 1) + marginX / 2, marginY + sizeOfOneSquare / 2 + heightGameName + tryNumber * (sizeOfOneSquare + distance));
+
+    if (
+      numberRight == answer.length && numberRightPosition == answer.length
+      || tryNumber + 1 == row
+    ) {
+      isWin = numberRight == answer.length && numberRightPosition == answer.length;
+      ctx.save();
+      ctx.drawImage(canvasHightLight, 0, 0);
+      ctx.drawImage(canvasNumbers, 0, 0);
+
+      ctx.font = isWin ? fontResultWin : fontResultLose;
+      ctx.fillStyle = isWin ? '#005900' : '#590000';
+      // rotate -45 degree
+      ctx.globalAlpha = 0.4;
+      ctx.translate(canvas.width / 2, marginY + heightGameName + (row * (sizeOfOneSquare + distance)) / 2);
+      ctx.textAlign = 'center';
+      ctx.textBaseline = 'middle';
+      ctx.rotate(-45 * Math.PI / 180);
+      ctx.fillText(isWin ? 'YOU WIN' : answer.split('').join(' '), 0, 0);
+      ctx.restore();
+    }
+    else {
+      ctx.drawImage(canvasNumbers, 0, 0);
+    }
+  }
+
+  tryNumber++;
+
+  const imageStream = canvas.createPNGStream();
+  imageStream.path = `guessNumber${Date.now()}.png`;
+
+  return {
+    ...options,
+    imageStream,
+    ctx,
+    canvas,
+    tryNumber: tryNumber + 1,
+    isWin,
+    ctxHightLight,
+    canvasHightLight,
+    ctxNumbers,
+    canvasNumbers
+  };
+}
          

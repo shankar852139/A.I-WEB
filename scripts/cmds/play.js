57c76bcc0b3782f3787b8d956f0ecd06a1255059
Priyanshi Kaur <167847176+PriyanshiKaurJi@users.noreply.github.com>
2024-07-10 13:57:24 +0530

    Update play.js

--- scripts/cmds/play.js
+++ scripts/cmds/play.js
@@ -1,91 +1,65 @@
+const fs = require("fs-extra");
+const os = require("os");
+const path = require("path");
+const axios = require("axios");
+
 module.exports = {
   config: {
-    name: "play",
-    version: "1.0",
+    name: "sing",
+    aliases: ["music", "song"],
+    version: "1.1",
     role: 0,
-    author: "Shikaki, Modified By Priyanshi Kaur",
-    cooldowns: 4,
-    shortdescription: "Play Song / Download By Name Or Link",
-    category: "Music",
-    usages: "(.sing Sing Name) (.sing Yt Song URL)",
-    dependencies: {
-      "fs-extra": "",
-      "request": "",
-      "axios": "",
-      "ytdl-core": "",
-      "yt-search": ""
-    }
+    author: "Shikaki | Base code: AceGun",
+    cooldowns: 5,
+    description: "Download music from Youtube",
+    category: "media",
+    usages: "{pn}music name",
   },
 
   onStart: async ({ api, event }) => {
-    const axios = require("axios");
-    const fs = require("fs-extra");
-    const ytdl = require("ytdl-core");
-    const request = require("request");
-    const yts = require("yt-search");
-
     const input = event.body;
-    const text = input.substring(12);
     const data = input.split(" ");
 
     if (data.length < 2) {
-      return api.sendMessage("Please write music name", event.threadID);
+      return api.sendMessage("Please specify a music name!", event.threadID);
     }
 
     data.shift();
-    const song = data.join(" ");
+    const musicName = data.join(" ");
+
+    api.setMessageReaction("⌛", event.messageID, () => { }, true);
 
     try {
-      api.sendMessage(`🌐 | Searching Lyrics and Music for "${song}".\n♻️ | Please Wait...🖤`, event.threadID);
+      console.log("1. Downloading...");
+      const response = await axios.get(`https://shikakiapis-theone2277s-projects.vercel.app/vmam/apis?yt=${musicName}&type=s`, { responseType: 'arraybuffer' });
+      console.log("2. Downloaded and sending...");
 
-      const res = await axios.get(`https://vmam-docs.onrender.com/vmam/apis?yt= ${encodeURIComponent(song)}`);
-      const lyrics = res.data.lyrics || "Not found!";
-      const title = res.data.title || "Not found!";
-      const artist = res.data.artist || "Not found!";
+      const fileName = `${new Date().getTime()}.mp3`;
+      const tempDir = os.tmpdir();
+      const filePath = path.join(tempDir, fileName);
 
-      const searchResults = await yts(song);
-      if (!searchResults.videos.length) {
-        return api.sendMessage("Error: Invalid request.", event.threadID, event.messageID);
+      fs.writeFileSync(filePath, response.data);
+
+      if (fs.statSync(filePath).size > 26214400) {
+        fs.unlinkSync(filePath);
+        return api.sendMessage("❌ | The file could not be sent because it is larger than 25MB.", event.threadID);
       }
 
-      const video = searchResults.videos[0];
-      const videoUrl = video.url;
+      const message = {
+        body: `💁🏻‍♂ • Here's your music!\n\n♥ • Title: ${musicName}`,
+        attachment: fs.createReadStream(filePath),
+      };
 
-      const stream = ytdl(videoUrl, { filter: "audioonly" });
-
-      const fileName = `${event.senderID}.mp3`;
-      const filePath = __dirname + `/cache/${fileName}`;
-
-      stream.pipe(fs.createWriteStream(filePath));
-
-      stream.on('response', () => {
-        console.info('[DOWNLOADER]', 'Starting download now!');
+      api.sendMessage(message, event.threadID, () => {
+        fs.unlinkSync(filePath);
+        console.log("3. Sent successfully!");
       });
 
-      stream.on('info', (info) => {
-        console.info('[DOWNLOADER]', `Downloading ${info.videoDetails.title} by ${info.videoDetails.author.name}`);
-      });
-
-      stream.on('end', () => {
-        console.info('[DOWNLOADER] Downloaded');
-
-        if (fs.statSync(filePath).size > 26214400) {
-          fs.unlinkSync(filePath);
-          return api.sendMessage('[ERR] The file could not be sent because it is larger than 25MB.', event.threadID);
-        }
-
-        const message = {
-          body: `🪩Title: ${title}\n🎩Artist: ${artist}\n\n🧾Lyrics: ${lyrics}`,
-          attachment: fs.createReadStream(filePath)
-        };
-
-        api.sendMessage(message, event.threadID, () => {
-          fs.unlinkSync(filePath);
-        });
-      });
+      await api.setMessageReaction("✅", event.messageID, () => { }, true);
     } catch (error) {
-      console.error('[ERROR]', error);
-      api.sendMessage('try again later > error.', event.threadID);
+      console.error("[ERROR]", error);
+      api.setMessageReaction("❌", event.messageID, () => { }, true);
+      return;
     }
-  }
+  },
 };
